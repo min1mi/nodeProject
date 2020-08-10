362a85965c630a0086942f984d450909e2c26cb8
min1mi <min2mi00@gmail.com>
2020-08-10 13:23:47 +0900

    first commit

--- workspace/nodeweb/webserver.js
+++ workspace/nodeweb/webserver.js
@@ -2,19 +2,38 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
+var home = path.join(__dirname, 'design');
+
 var server = http.createServer(function(req, res){
   console.log(req.method, req.url, req.httpVersion);
   console.log(req.headers);
 
   var filename = req.url.substring(1);
   
-  // __dirname : 현재 파일 절대경로
-  var data = fs.readFileSync(path.join(__dirname, filename));
+  // 비동기 방식
+  fs.readFile(path.join(home, filename), function(err, data) {
+    if(err) {
+      console.error(err.message);
+      res.writeHead(404, {'Content-Type':'text/html;charset=utf-8'});
+      res.end('<h1>' + filename + ' Not found!!!</h1>');
+    } else {
+      res.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
+      res.end(data);
+    }
+  });
 
+  // 동기 방식
+  try {
+    // __dirname : 현재 파일 절대경로
+    var data = fs.readFileSync(path.join(home, filename));
+    res.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
+    // res.end('<h1>Hello HTTP Server</h1>');
+    res.end(data);
+  } catch(err) {
+    res.writeHead(404, {'Content-Type':'text/html;charset=utf-8'});
+    res.end('<h1>' + filename + ' Not found!!!</h1>');
+  }
 
-  res.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
-  // res.end('<h1>Hello HTTP Server</h1>');
-  res.end(data);
 });
 
 server.listen(80, function(){
